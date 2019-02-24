4969cb673e7c0cef5813f71b0982ccc4cf0aaed8
lvxinup <1564605677@qq.com>
2019-02-24 19:27:00 +0800

    update

---
+++ 回文判断/回文判断/test.cpp
@@ -0,0 +1,31 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<string>
+#include<Windows.h>
+
+using namespace std;
+
+int main()
+{
+	//string s;
+	//cin >> s  ;
+	//s += s[0];
+	//string p(s);
+	//size_t  start = 0;
+	//size_t end = s.size() - 1;
+	//while (start < end)
+	//{
+	//	swap(s[start], s[end]);
+	//	++start;
+	//	--end;
+	//}
+	//if (s == p)
+	//	cout << "YES" << endl;
+	//else
+	//	cout << "NO" << endl;
+
+
+	system("pause");
+	return 0;
+}

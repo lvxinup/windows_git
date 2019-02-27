3ae2d12f98c3dbb5c0d2649e57be0a3ad685f8fc
lvxinup <1564605677@qq.com>
2019-02-27 23:02:13 +0800

    update

---
+++ C++11新特性/C++11新特性/test.cpp
@@ -0,0 +1,62 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<Windows.h>
+#include<time.h>
+
+using namespace std;
+
+//struct A
+//{
+//	int a[10000];
+//};
+//
+//void TestFunc1(A* a)
+//{}
+//
+//void TestFunc2(A& a)
+//{}
+//
+//void Test()	//引用和指针做参数的对比
+//{
+//	A a;
+//
+//	size_t begin1 = clock();
+//	for (int i = 0; i < 10000; i++)
+//	{
+//		TestFunc1(&a);
+//	}
+//	size_t end1 = clock();
+//
+//	size_t begin2 = clock();
+//	for (int j = 0; j < 10000; j++)
+//		TestFunc2(a);
+//	size_t end2 = clock();
+//
+//	cout << "指针做参数：" << end1 - begin1 << endl;
+//	cout << "引用做参数：" << end2 - begin2 << endl;
+//}
+
+
+
+int main()
+{
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	Test();
+	//}
+
+
+	//int a = 10;
+	//int& ra = a;
+
+	//cout << "&a= " << &a << endl;
+	//cout << "&ra= " << &ra << endl;
+
+
+
+
+	system("pause");
+	return 0;
+
+}
